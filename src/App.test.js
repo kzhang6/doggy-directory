55ce953d53a3346dcb3b91cca501df63a3ecd02c
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-16 22:05:14 -0700

    Test landing page

--- src/App.test.js
+++ src/App.test.js
@@ -2,5 +2,12 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders the landing page", () => {
-  render(<App />);
+  render(<App />);  //render the component into DOM
+
+  /* test if the landing page renders accurately
+     expect function paired with a matcher function */
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
 });
